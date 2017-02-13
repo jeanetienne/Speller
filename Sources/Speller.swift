ea30ef04e1d829d49487986eb42dbec84666ed1b
Jean-Étienne <git@jeanetienne.net>
2017-02-13 22:31:57 +1100

    Speller can describe Emojis

--- Sources/Speller.swift
+++ Sources/Speller.swift
@@ -18,10 +18,12 @@ public class Speller {
     ///   - alphabet: The spelling alphabet to be used to spell out the input phrase
     /// - Returns: An array of `SpelledCharacter`s describing each character of the input phrase
     public static func spell(phrase: String, withSpellingAlphabet alphabet: SpellingAlphabet) -> [SpelledCharacter] {
-        return phrase.characters.map { character -> SpelledCharacter in
+        let spelling = phrase.characters.map { character -> SpelledCharacter in
             return spell(character: character,
                          withSpellingAlphabet: alphabet)
         }
+
+        return describeUnknownCharacters(inSpelling: spelling)
     }
 
     // MARK: - Private methods
@@ -42,5 +44,29 @@ public class Speller {
 
         return codeWord
     }
-    
+
+    private static func describeUnknownCharacters(inSpelling spelling: [SpelledCharacter]) -> [SpelledCharacter] {
+        guard let characterDescriptor = CharacterDescriptor(.Emoji) else {
+            return spelling
+        }
+
+        return spelling.map { spelledCharacter -> SpelledCharacter in
+            describe(spelledCharacter: spelledCharacter, withCharacterDescriptor: characterDescriptor)
+        }
+    }
+
+    private static func describe(spelledCharacter: SpelledCharacter,
+                                 withCharacterDescriptor characterDescriptor: CharacterDescriptor) -> SpelledCharacter {
+        switch spelledCharacter {
+        case let .Unknown(unknownCharacter):
+            if let description = characterDescriptor.description("\(unknownCharacter)") {
+                return SpelledCharacter.Description(unknownCharacter, description)
+            } else {
+                return spelledCharacter
+            }
+        default:
+            return spelledCharacter
+        }
+    }
+
 }
