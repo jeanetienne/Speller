6e81f34097597cb03f5cb8808b1d321a741f2261
Jean-Étienne <git@jeanetienne.net>
2017-02-19 23:04:47 +1100

    Unit tests for Speller and SpellingAlphabet

--- Tests/SpellerTests/SpellerTests.swift
+++ Tests/SpellerTests/SpellerTests.swift
@@ -23,7 +23,7 @@ class SpellerTests: XCTestCase {
         XCTAssertEqual(spelling.count, word.characters.count)
     }
     
-    func testSpellingAllLettersAccurately() {
+    func testSpellingAllLettersAccuratelyWithInternationalRadiotelephony() {
         let phrase = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .InternationalRadiotelephony)
         let control = [SpelledCharacter.Match("a", "Alfa"),
@@ -82,6 +82,330 @@ class SpellerTests: XCTestCase {
         XCTAssertEqual(spelling, control, "Spelling the latin alphabet with the International Radiotelephoney spelling alphabet has failed")
     }
 
+    func testSpellingCzech() {
+        let phrase = "České Budějovice"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .Czech)
+        let control = [
+            SpelledCharacter.Match("Č", "Cyril"),
+            SpelledCharacter.Match("e", "Emil"),
+            SpelledCharacter.Match("s", "Svatopluk"),
+            SpelledCharacter.Match("k", "Karel"),
+            SpelledCharacter.Match("é", "Emil"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Match("B", "Božena"),
+            SpelledCharacter.Match("u", "Urban"),
+            SpelledCharacter.Match("d", "David"),
+            SpelledCharacter.Match("ě", "Emil"),
+            SpelledCharacter.Match("j", "Josef"),
+            SpelledCharacter.Match("o", "Otakar"),
+            SpelledCharacter.Match("v", "Václav"),
+            SpelledCharacter.Match("i", "Ivan"),
+            SpelledCharacter.Match("c", "Cyril"),
+            SpelledCharacter.Match("e", "Emil")
+        ]
+
+        XCTAssertEqual(spelling, control, "Czech spelling is wrong")
+    }
+
+    func testSpellingDanish() {
+        let phrase = "ægir & sørenå"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .Danish)
+        let control = [
+            SpelledCharacter.Match("æ", "Ægir"),
+            SpelledCharacter.Match("g", "Georg"),
+            SpelledCharacter.Match("i", "Ida"),
+            SpelledCharacter.Match("r", "Rasmus"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Description("&", "Ampersand"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Match("s", "Søren"),
+            SpelledCharacter.Match("ø", "Øresund"),
+            SpelledCharacter.Match("r", "Rasmus"),
+            SpelledCharacter.Match("e", "Erik"),
+            SpelledCharacter.Match("n", "Nikolaj"),
+            SpelledCharacter.Match("å", "Aase")
+        ]
+
+        XCTAssertEqual(spelling, control, "Danish spelling is wrong")
+    }
+
+    func testSpellingDutch() {
+        let phrase = "Rĳksmuseum"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .Dutch)
+        let control = [
+            SpelledCharacter.Match("R", "Richard"),
+            SpelledCharacter.Match("ĳ", "Ĳmuiden"),
+            SpelledCharacter.Match("k", "Karel"),
+            SpelledCharacter.Match("s", "Simon"),
+            SpelledCharacter.Match("m", "Maria"),
+            SpelledCharacter.Match("u", "Utrecht"),
+            SpelledCharacter.Match("s", "Simon"),
+            SpelledCharacter.Match("e", "Eduard"),
+            SpelledCharacter.Match("u", "Utrecht"),
+            SpelledCharacter.Match("m", "Maria")
+        ]
+
+        XCTAssertEqual(spelling, control, "Dutch spelling is wrong")
+    }
+
+    func testSpellingFinnish() {
+        let phrase = "öå-äiti"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .Finnish)
+        let control = [
+            SpelledCharacter.Match("ö", "Öljy"),
+            SpelledCharacter.Match("å", "Åke"),
+            SpelledCharacter.Description("-", "Hyphen-minus"),
+            SpelledCharacter.Match("ä", "Äiti"),
+            SpelledCharacter.Match("i", "Iivari"),
+            SpelledCharacter.Match("t", "Tyyne"),
+            SpelledCharacter.Match("i", "Iivari")
+        ]
+
+        XCTAssertEqual(spelling, control, "Finnish spelling is wrong")
+    }
+
+    func testSpellingFrench() {
+        let phrase = "Montpellier"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .French)
+        let control = [
+            SpelledCharacter.Match("M", "Marcel"),
+            SpelledCharacter.Match("o", "Oscar"),
+            SpelledCharacter.Match("n", "Nicolas"),
+            SpelledCharacter.Match("t", "Thérèse"),
+            SpelledCharacter.Match("p", "Pierre"),
+            SpelledCharacter.Match("e", "Eugène"),
+            SpelledCharacter.Match("l", "Louis"),
+            SpelledCharacter.Match("l", "Louis"),
+            SpelledCharacter.Match("i", "Irma"),
+            SpelledCharacter.Match("e", "Eugène"),
+            SpelledCharacter.Match("r", "Raoul")
+        ]
+
+        XCTAssertEqual(spelling, control, "French spelling is wrong")
+    }
+
+    func testSpellingGerman() {
+        let phrase = "Möbelträgerfüße"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .German)
+        let control = [
+            SpelledCharacter.Match("M", "Martha"),
+            SpelledCharacter.Match("ö", "Ökonom"),
+            SpelledCharacter.Match("b", "Berta"),
+            SpelledCharacter.Match("e", "Emil"),
+            SpelledCharacter.Match("l", "Ludwig"),
+            SpelledCharacter.Match("t", "Theodor"),
+            SpelledCharacter.Match("r", "Richard"),
+            SpelledCharacter.Match("ä", "Ärger"),
+            SpelledCharacter.Match("g", "Gustav"),
+            SpelledCharacter.Match("e", "Emil"),
+            SpelledCharacter.Match("r", "Richard"),
+            SpelledCharacter.Match("f", "Friedrich"),
+            SpelledCharacter.Match("ü", ["Übermut", "Übel"]),
+            SpelledCharacter.Match("ß", "Eszett"),
+            SpelledCharacter.Match("e", "Emil")
+        ]
+
+        XCTAssertEqual(spelling, control, "German spelling is wrong")
+    }
+
+    func testSpellingItalian() {
+        let phrase = "Xilofono"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .Italian)
+        let control = [
+            SpelledCharacter.Match("X", ["Xeres", "Xilofono"]),
+            SpelledCharacter.Match("i", "Imola"),
+            SpelledCharacter.Match("l", "Livorno"),
+            SpelledCharacter.Match("o", "Otranto"),
+            SpelledCharacter.Match("f", "Firenze"),
+            SpelledCharacter.Match("o", "Otranto"),
+            SpelledCharacter.Match("n", "Napoli"),
+            SpelledCharacter.Match("o", "Otranto")
+        ]
+
+        XCTAssertEqual(spelling, control, "Italian spelling is wrong")
+    }
+
+    func testSpellingNorwegian() {
+        let phrase = "åse, ærlig & østen"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .Norwegian)
+        let control = [
+            SpelledCharacter.Match("å", "Åse"),
+            SpelledCharacter.Match("s", "Sigrid"),
+            SpelledCharacter.Match("e", "Edith"),
+            SpelledCharacter.Description(",", "Comma"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Match("æ", "Ærlig"),
+            SpelledCharacter.Match("r", "Rikard"),
+            SpelledCharacter.Match("l", "Ludvig"),
+            SpelledCharacter.Match("i", "Ivar"),
+            SpelledCharacter.Match("g", "Gustav"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Description("&", "Ampersand"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Match("ø", "Østen"),
+            SpelledCharacter.Match("s", "Sigrid"),
+            SpelledCharacter.Match("t", "Teodor"),
+            SpelledCharacter.Match("e", "Edith"),
+            SpelledCharacter.Match("n", "Nils")
+        ]
+
+        XCTAssertEqual(spelling, control, "Norwegian spelling is wrong")
+    }
+
+    func testSpellingPortuguese() {
+        let phrase = "Lisboa"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .Portuguese)
+        let control = [
+            SpelledCharacter.Match("L", "Lisboa"),
+            SpelledCharacter.Match("i", "Itália"),
+            SpelledCharacter.Match("s", "Setúbal"),
+            SpelledCharacter.Match("b", "Braga"),
+            SpelledCharacter.Match("o", "Ovar"),
+            SpelledCharacter.Match("a", "Aveiro")
+        ]
+
+        XCTAssertEqual(spelling, control, "Portuguese spelling is wrong")
+    }
+
+    func testSpellingPortugueseBrazilian() {
+        let phrase = "Rio de Janeiro"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .PortugueseBrazilian)
+        let control = [
+            SpelledCharacter.Match("R", "Raiz"),
+            SpelledCharacter.Match("i", "Índio"),
+            SpelledCharacter.Match("o", "Ouro"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Match("d", "Dado"),
+            SpelledCharacter.Match("e", "Estrela"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Match("J", "José"),
+            SpelledCharacter.Match("a", "Amor"),
+            SpelledCharacter.Match("n", "Navio"),
+            SpelledCharacter.Match("e", "Estrela"),
+            SpelledCharacter.Match("i", "Índio"),
+            SpelledCharacter.Match("r", "Raiz"),
+            SpelledCharacter.Match("o", "Ouro")
+        ]
+
+        XCTAssertEqual(spelling, control, "Portuguese (Brazilian) spelling is wrong")
+    }
+
+    func testSpellingSlovene() {
+        let phrase = "Ajdovščina & Tržič"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .Slovene)
+        let control = [
+            SpelledCharacter.Match("A", "Ankaran"),
+            SpelledCharacter.Match("j", "Jadran"),
+            SpelledCharacter.Match("d", "Drava"),
+            SpelledCharacter.Match("o", "Ormož"),
+            SpelledCharacter.Match("v", "Velenje"),
+            SpelledCharacter.Match("š", "Šmarje"),
+            SpelledCharacter.Match("č", "Čatež"),
+            SpelledCharacter.Match("i", "Izola"),
+            SpelledCharacter.Match("n", "Nanos"),
+            SpelledCharacter.Match("a", "Ankaran"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Description("&", "Ampersand"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Match("T", "Triglav"),
+            SpelledCharacter.Match("r", "Ravne"),
+            SpelledCharacter.Match("ž", "Žalec"),
+            SpelledCharacter.Match("i", "Izola"),
+            SpelledCharacter.Match("č", "Čatež"),
+        ]
+
+        XCTAssertEqual(spelling, control, "Slovene spelling is wrong")
+    }
+
+    func testSpellingSpanish() {
+        let phrase = "Bañar"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .Spanish)
+        let control = [
+            SpelledCharacter.Match("B", "Burgos"),
+            SpelledCharacter.Match("a", "Antonio"),
+            SpelledCharacter.Match("ñ", "Ñoño"),
+            SpelledCharacter.Match("a", "Antonio"),
+            SpelledCharacter.Match("r", "Ramón")
+        ]
+
+        XCTAssertEqual(spelling, control, "Spanish spelling is wrong")
+    }
+
+    func testSpellingSwedish() {
+        let phrase = "Åmål, Höganäs & übel"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .Swedish)
+        let control = [
+            SpelledCharacter.Match("Å", "Åke"),
+            SpelledCharacter.Match("m", "Martin"),
+            SpelledCharacter.Match("å", "Åke"),
+            SpelledCharacter.Match("l", "Ludvig"),
+            SpelledCharacter.Description(",", "Comma"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Match("H", "Helge"),
+            SpelledCharacter.Match("ö", "Östen"),
+            SpelledCharacter.Match("g", "Gustav"),
+            SpelledCharacter.Match("a", "Adam"),
+            SpelledCharacter.Match("n", "Niklas"),
+            SpelledCharacter.Match("ä", "Ärlig"),
+            SpelledCharacter.Match("s", "Sigurd"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Description("&", "Ampersand"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Match("ü", "Übel"),
+            SpelledCharacter.Match("b", "Bertil"),
+            SpelledCharacter.Match("e", "Erik"),
+            SpelledCharacter.Match("l", "Ludvig")
+        ]
+        
+        XCTAssertEqual(spelling, control, "Swedish spelling is wrong")
+    }
+    
+    func testSpellingTurkish() {
+        let phrase = "Istanbul, Elâzığ, Uşak, İnegöl, Düziçi"
+        let spelling = Speller.spell(phrase: phrase, withSpellingAlphabet: .Turkish)
+        let control = [
+            SpelledCharacter.Match("I", "Isparta"),
+            SpelledCharacter.Match("s", "Sinop"),
+            SpelledCharacter.Match("t", "Tokat"),
+            SpelledCharacter.Match("a", "Adana"),
+            SpelledCharacter.Match("n", "Niğde"),
+            SpelledCharacter.Match("b", "Bolu"),
+            SpelledCharacter.Match("u", "Uşak"),
+            SpelledCharacter.Match("l", "Lüleburgaz"),
+            SpelledCharacter.Description(",", "Comma"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Match("E", "Edirne"),
+            SpelledCharacter.Match("l", "Lüleburgaz"),
+            SpelledCharacter.Match("â", "Adana"),
+            SpelledCharacter.Match("z", "Zonguldak"),
+            SpelledCharacter.Match("ı", "Isparta"),
+            SpelledCharacter.Match("ğ", "Yumuşak G"),
+            SpelledCharacter.Description(",", "Comma"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Match("U", "Uşak"),
+            SpelledCharacter.Match("ş", "Şırnak"),
+            SpelledCharacter.Match("a", "Adana"),
+            SpelledCharacter.Match("k", "Kars"),
+            SpelledCharacter.Description(",", "Comma"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Match("İ", "İzmir"),
+            SpelledCharacter.Match("n", "Niğde"),
+            SpelledCharacter.Match("e", "Edirne"),
+            SpelledCharacter.Match("g", "Giresun"),
+            SpelledCharacter.Match("ö", "Ödemiş"),
+            SpelledCharacter.Match("l", "Lüleburgaz"),
+            SpelledCharacter.Description(",", "Comma"),
+            SpelledCharacter.Description(" ", "Space"),
+            SpelledCharacter.Match("D", "Denizli"),
+            SpelledCharacter.Match("ü", "Ünye"),
+            SpelledCharacter.Match("z", "Zonguldak"),
+            SpelledCharacter.Match("i", "Isparta"),
+            SpelledCharacter.Match("ç", "Çanakkale"),
+            SpelledCharacter.Match("i", "Isparta")
+        ]
+        
+        XCTAssertEqual(spelling, control, "Turkish spelling is wrong")
+    }
 
     func testDescribesUnknownCharacters() {
         let phrase = "ABC 🦆🦎🦈 !@#$%^&*()"
--- Tests/SpellerTests/SpellingAlphabetTests.swift
+++ Tests/SpellerTests/SpellingAlphabetTests.swift
@@ -75,6 +75,91 @@ class SpellingAlphabetTests: XCTestCase {
         XCTAssertEqual(content, control, "LAPD spelling alphabet has changed")
     }
 
+    func testCzech() {
+        let content = SpellingAlphabet.Czech.content
+        let control: SpellingAlphabetContent =
+            ["A": "Adam",      "B": "Božena",
+             "C": "Cyril",     "D": "David",
+             "E": "Emil",      "F": "František",
+             "G": "Gustav",    "H": "Helena",
+             "I": "Ivan",      "J": "Josef",
+             "K": "Karel",     "L": "Ludvik",
+             "M": "Marie",     "N": "Norbert",
+             "O": "Otakar",    "P": "Petr",
+             "Q": "Quido",     "R": "Rudolf",
+             "S": "Svatopluk", "T": "Tomáš",
+             "U": "Urban",     "V": "Václav",
+             "W": "Dvojité V", "X": "Xaver",
+             "Y": "Ypsilon",   "Z": "Zuzana"]
+
+        XCTAssertEqual(content, control, "Czech spelling alphabet has changed")
+    }
+
+    func testDanish() {
+        let content = SpellingAlphabet.Danish.content
+        let control: SpellingAlphabetContent =
+            ["A": "Anna",    "Å": "Aase",
+             "Æ": "Ægir",    "B": "Bernhard",
+             "C": "Cecilie", "D": "David",
+             "E": "Erik",    "F": "Frederik",
+             "G": "Georg",   "H": "Hans",
+             "I": "Ida",     "J": "Johan",
+             "K": "Karen",   "L": "Ludvig",
+             "M": "Mari",    "N": "Nikolaj",
+             "O": "Odin",    "Ø": "Øresund",
+             "P": "Peter",   "Q": "Quintus",
+             "R": "Rasmus",  "S": "Søren",
+             "T": "Theodor", "U": "Ulla",
+             "V": "Viggo",   "W": "William",
+             "X": "Xerxes",  "Y": "Yrsa",
+             "Z": "Zackarias"]
+
+        XCTAssertEqual(content, control, "Danish spelling alphabet has changed")
+    }
+
+    func testDutch() {
+        let content = SpellingAlphabet.Dutch.content
+        let control: SpellingAlphabetContent =
+            ["A": "Anton",    "B": "Bernard",
+             "C": "Cornelis", "D": "Dirk",
+             "E": "Eduard",   "F": "Ferdinand",
+             "G": "Gerard",   "H": "Hendrik",
+             "I": "Izaak",    "J": "Julius",
+             "K": "Karel",    "L": "Lodewijk",
+             "M": "Maria",    "N": "Nico",
+             "O": "Otto",     "P": "Pieter",
+             "Q": "Quotiënt", "R": "Richard",
+             "S": "Simon",    "T": "Theodor",
+             "U": "Utrecht",  "V": "Victor",
+             "W": "Willem",   "X": "Xanthippe",
+             "Ĳ": "Ĳmuiden",  "Y": "Ypsilon",
+             "Z": "Zaandam"]
+
+        XCTAssertEqual(content, control, "Dutch spelling alphabet has changed")
+    }
+
+    func testFinnish() {
+        let content = SpellingAlphabet.Finnish.content
+        let control: SpellingAlphabetContent =
+            ["A": "Aarne",   "Ä": "Äiti",
+             "Å": "Åke",     "B": "Bertta",
+             "C": "Celsius", "D": "Daavid",
+             "E": "Eemeli",  "F": "Faarao",
+             "G": "Gideon",  "H": "Heikki",
+             "I": "Iivari",  "J": "Jussi",
+             "K": "Kalle",   "L": "Lauri",
+             "M": "Matti",   "N": "Niilo",
+             "O": "Otto",    "Ö": "Öljy",
+             "P": "Paavo",   "Q": "Kuu",
+             "R": "Risto",   "S": "Sakari",
+             "T": "Tyyne",   "U": "Urho",
+             "V": "Vihtori", "W": "Wiski",
+             "X": "Äksä",    "Y": "Yrjö",
+             "Z": "Tseta"]
+
+        XCTAssertEqual(content, control, "Finnish spelling alphabet has changed")
+    }
+
     func testFrench() {
         let content = SpellingAlphabet.French.content
         let control: SpellingAlphabetContent =
@@ -93,6 +178,197 @@ class SpellingAlphabetTests: XCTestCase {
              "Y": "Yvonne",      "Z": "Zoé"]
 
         XCTAssertEqual(content, control, "French spelling alphabet has changed")
+    }
+
+    func testGerman() {
+        let content = SpellingAlphabet.German.content
+        let control: SpellingAlphabetContent =
+            ["A": "Anton",                 "Ä": "Ärger",
+             "B": "Berta",                 "C": "Cäsar",
+             "D": "Dora",                  "E": "Emil",
+             "F": "Friedrich",             "G": "Gustav",
+             "H": "Heinrich",              "I": "Ida",
+             "J": "Julius",                "K": ["Kaufmann", "Konrad"],
+             "L": "Ludwig",                "M": "Martha",
+             "N": "Nordpol",               "O": "Otto",
+             "Ö": "Ökonom",                "P": "Paula",
+             "Q": "Quelle",                "R": "Richard",
+             "S": ["Samuel", "Siegfried"], "ß": "Eszett",
+             "T": "Theodor",               "U": "Ulrich",
+             "Ü": ["Übermut", "Übel"],     "V": "Viktor",
+             "W": "Wilhelm",               "X": ["Xanthippe", "Xaver"],
+             "Y": "Ypsilon",               "Z": ["Zacharias", "Zürich"]]
+
+        XCTAssertEqual(content, control, "German spelling alphabet has changed")
+    }
+
+    func testItalian() {
+        let content = SpellingAlphabet.Italian.content
+        let control: SpellingAlphabetContent =
+            ["A": "Ancona",          "B": "Bologna",
+             "C": "Como",            "D": "Domodossola",
+             "E": "Empoli",          "F": "Firenze",
+             "G": "Genova",          "H": "Hotel",
+             "I": "Imola",           "J": "Jolly",
+             "K": "Kappa",           "L": "Livorno",
+             "M": "Milano",          "N": "Napoli",
+             "O": "Otranto",         "P": "Padova",
+             "Q": "Quadro",          "R": "Roma",
+             "S": "Savona",          "T": "Torino",
+             "U": "Udine",           "V": "Venezia",
+             "W": "Washington",      "X": ["Xeres", "Xilofono"],
+             "Y": ["York", "Yacht"], "Z": "Zara"]
+
+        XCTAssertEqual(content, control, "Italian spelling alphabet has changed")
+    }
+
+    func testNorwegian() {
+        let content = SpellingAlphabet.Norwegian.content
+        let control: SpellingAlphabetContent =
+            ["A": "Anna",     "Å": "Åse",
+             "Æ": "Ærlig",    "B": "Bernhard",
+             "C": "Caesar",   "D": "David",
+             "E": "Edith",    "F": "Fredrik",
+             "G": "Gustav",   "H": "Harald",
+             "I": "Ivar",     "J": "Johan",
+             "K": "Karin",    "L": "Ludvig",
+             "M": "Martin",   "N": "Nils",
+             "O": "Olivia",   "Ø": "Østen",
+             "P": "Petter",   "Q": "Quintus",
+             "R": "Rikard",   "S": "Sigrid",
+             "T": "Teodor",   "U": "Ulrik",
+             "V": "Enkelt-V", "W": "Dobbelt-V",
+             "X": "Xerxes",   "Y": "Yngling",
+             "Z": "Zakarias"]
+
+        XCTAssertEqual(content, control, "Norwegian spelling alphabet has changed")
+    }
+
+    func testPortuguese() {
+        let content = SpellingAlphabet.Portuguese.content
+        let control: SpellingAlphabetContent =
+            ["A": "Aveiro",   "B": "Braga",
+             "C": "Coimbra",  "D": "Dafundo",
+             "E": "Évora",    "F": "Faro",
+             "G": "Guarda",   "H": "Horta",
+             "I": "Itália",   "J": "José",
+             "K": "Kodak",    "L": "Lisboa",
+             "M": "Maria",    "N": "Nazaré",
+             "O": "Ovar",     "P": "Porto",
+             "Q": "Queluz",   "R": "Rossio",
+             "S": "Setúbal",  "T": "Tavira",
+             "U": "Unidade",  "V": "Vidago",
+             "W": "Waldemar", "X": "Xavier",
+             "Y": "York",     "Z": "Zulmira"]
+
+        XCTAssertEqual(content, control, "Portuguese spelling alphabet has changed")
+    }
+
+    func testPortugueseBrazilian() {
+        let content = SpellingAlphabet.PortugueseBrazilian.content
+        let control: SpellingAlphabetContent =
+            ["A": "Amor",     "B": "Bandeira",
+             "C": "Cobra",    "D": "Dado",
+             "E": "Estrela",  "F": "Feira",
+             "G": "Goiaba",   "H": "Hotel",
+             "I": "Índio",    "J": "José",
+             "K": "Kiwi",     "L": "Lua",
+             "M": "Maria",    "N": "Navio",
+             "O": "Ouro",     "P": "Pipa",
+             "Q": "Quilombo", "R": "Raiz",
+             "S": "Saci",     "T": "Tatu",
+             "U": "Uva",      "V": "Vitória",
+             "W": "Wilson",   "X": "Xadrez",
+             "Y": "Yolanda",  "Z": "Zebra"]
+
+        XCTAssertEqual(content, control, "Portuguese (Brazilian) spelling alphabet has changed")
+    }
+
+    func testSlovene() {
+        let content = SpellingAlphabet.Slovene.content
+        let control: SpellingAlphabetContent =
+            ["A": "Ankaran",   "B": "Bled",
+             "C": "Celje",     "Č": "Čatež",
+             "D": "Drava",     "E": "Evropa",
+             "F": "Fala",      "G": "Gorica",
+             "H": "Hrastnik",  "I": "Izola",
+             "J": "Jadran",    "K": "Kamnik",
+             "L": "Ljubljana", "M": "Maribor",
+             "N": "Nanos",     "O": "Ormož",
+             "P": "Piran",     "Q": "Queen",
+             "R": "Ravne",     "S": "Soča",
+             "Š": "Šmarje",    "T": "Triglav",
+             "U": "Unec",      "V": "Velenje",
+             "W": "Dvojni V",  "X": "Iks",
+             "Y": "Ipsilon",   "Z": "Zalog",
+             "Ž": "Žalec"]
+
+        XCTAssertEqual(content, control, "Slovene spelling alphabet has changed")
+    }
+
+    func testSpanish() {
+        let content = SpellingAlphabet.Spanish.content
+        let control: SpellingAlphabetContent =
+            ["A": "Antonio",  "B": "Burgos",
+             "C": "Carmen",   "D": "Dolores",
+             "E": "Enrique",  "F": "Francia",
+             "G": "Granada",  "H": "Historia",
+             "I": "Inés",     "J": "José",
+             "K": "Kilo",     "L": "Lorenzo",
+             "M": "Madrid",   "N": "Navarra",
+             "Ñ": "Ñoño",     "O": "Oviedo",
+             "P": "París",    "Q": "Querido",
+             "R": "Ramón",    "S": "Sábado",
+             "T": "Toledo",   "U": "Ulises",
+             "V": "Valencia", "W": "Washington",
+             "X": "Xilófono", "Y": "Yegua",
+             "Z": "Zaragoza"]
+
+        XCTAssertEqual(content, control, "Spanish spelling alphabet has changed")
+    }
+
+    func testSwedish() {
+        let content = SpellingAlphabet.Swedish.content
+        let control: SpellingAlphabetContent =
+            ["A": "Adam",    "Ä": "Ärlig",
+             "Å": "Åke",     "B": "Bertil",
+             "C": "Caesar",  "D": "David",
+             "E": "Erik",    "F": "Filip",
+             "G": "Gustav",  "H": "Helge",
+             "I": "Ivar",    "J": "Johan",
+             "K": "Kalle",   "L": "Ludvig",
+             "M": "Martin",  "N": "Niklas",
+             "O": "Olof",    "Ö": "Östen",
+             "P": "Petter",  "Q": "Quintus",
+             "R": "Rudolf",  "S": "Sigurd",
+             "T": "Tore",    "U": "Urban",
+             "Ü": "Übel",    "V": "Viktor",
+             "W": "Wilhelm", "X": "Xerxes",
+             "Y": "Yngve",   "Z": "Zäta"]
+        
+        XCTAssertEqual(content, control, "Swedish spelling alphabet has changed")
+    }
+    
+    func testTurkish() {
+        let content = SpellingAlphabet.Turkish.content
+        let control: SpellingAlphabetContent =
+            ["A": "Adana",      "B": "Bolu",
+             "C": "Ceyhan",     "Ç": "Çanakkale",
+             "D": "Denizli",    "E": "Edirne",
+             "F": "Fatsa",      "G": "Giresun",
+             "Ğ": "Yumuşak G",  "H": "Hatay",
+             "I": "Isparta",    "İ": "İzmir",
+             "J": "Jandarma",   "K": "Kars",
+             "L": "Lüleburgaz", "M": "Muş",
+             "N": "Niğde",      "O": "Ordu",
+             "Ö": "Ödemiş",     "P": "Polatlı",
+             "R": "Rize",       "S": "Sinop",
+             "Ş": "Şırnak",     "T": "Tokat",
+             "U": "Uşak",       "Ü": "Ünye",
+             "V": "Van",        "Y": "Yozgat",
+             "Z": "Zonguldak"]
+        
+        XCTAssertEqual(content, control, "Turkish spelling alphabet has changed")
     }
 
 }
